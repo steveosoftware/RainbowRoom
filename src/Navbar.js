5d0e47045c69ec6de9cdd7e40acedfe35ae2bac9
SteveO <stinkypiece@yahoo.com>
2021-04-27 14:11:06 -0700

    add footer and fix navbar in singlecolor palette component

--- src/Navbar.js
+++ src/Navbar.js
@@ -36,18 +36,20 @@ class Navbar extends Component {
         <div className="logo">
           <Link to="/" >reactcolorpicker</Link>
         </div>
+        {this.props.showingAllColors && (
           <div className="slider-container">
-            <span>Level: {level}</span>
-              <div className="slider">
-              <Slider
-                defaultValue={level}
-                min={100}
-                max={900}
-                step={100}
-                onAfterChange={changeLevel}
-              />
-            </div>
+          <span>Level: {level}</span>
+            <div className="slider">
+            <Slider
+              defaultValue={level}
+              min={100}
+              max={900}
+              step={100}
+              onAfterChange={changeLevel}
+            />
           </div>
+          </div>
+        )}
           <div className="select-container">
             <Select value={format} onChange={this.handleFormatChange}>
               <MenuItem value="hex">Hex - #ffffff</MenuItem>
--- src/Palette.js
+++ src/Palette.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import './Palette.css';
+import PaletteFooter from './PaletteFooter';
 
 
 class Palette extends Component {
@@ -39,12 +40,11 @@ class Palette extends Component {
           level={level}
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
-          text={level}/>
+          text={level}
+          showingAllColors
+          />
         <div className="Palette-colors">{colorBoxes}</div>
-        <footer className="Palette-footer">
-          {paletteName}
-          <span className="emoji">{emoji}</span>
-        </footer>
+        <PaletteFooter paletteName={paletteName} emoji={emoji}/>
       </div>
     )
   }
