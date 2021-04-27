fd73a25cad032e5a5947c95ebaf31f019aafb329
SteveO <stinkypiece@yahoo.com>
2021-04-27 12:12:46 -0700

    add link and route to colorbox

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
           path="/palette/:id"
           render={routeProps =>(
           <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} /> )}/>
+          <Route path="/palette/:paletteId/:colorId" render={() => <h1>Single Color Page</h1>}/>
       </Switch>
       // <div className="App">
       //   <Palette palette={generatePalette(seedColors[4])} />
