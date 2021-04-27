bd2d83278b13397a094c55c004aa176d88d6be84
SteveO <stinkypiece@yahoo.com>
2021-04-27 12:02:01 -0700

    add link to palette with history.push and routeProps

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
 
     return (
       <Switch>
-        <Route exact path="/" render={() => <PaletteList palettes={seedColors} /> }/>
+        <Route exact path="/" render={routeProps => <PaletteList {...routeProps} palettes={seedColors} /> }/>
         <Route
           exact
           path="/palette/:id"
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -37,7 +37,9 @@ const styles = {
 
 class PaletteList extends Component {
 
-
+  goToPalette(id){
+    this.props.history.push(`/palette/${id}`)
+  }
   render(){
     const {palettes, classes } = this.props;
     return(
@@ -48,7 +50,7 @@ class PaletteList extends Component {
           </nav>
           <div className={classes.palettes}>
             {palettes.map(p => (
-              <MiniPalette {...p}/>
+              <MiniPalette {...p} handleClick={()=> this.goToPalette(p.id)}/>
             ))}
           </div>
         </div>
