b28c8cb5b79d4a3986bd9e48c071384e9a1f5786
SteveO <stinkypiece@yahoo.com>
2021-04-27 19:33:05 -0700

    complete back button and styling single color palette

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,7 +1,9 @@
 import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
+
 
 class SingleColorPalette extends PureComponent {
   constructor(props) {
@@ -28,19 +30,22 @@ class SingleColorPalette extends PureComponent {
 
   render() {
     const { format } = this.state
-    const { paletteName, emoji } = this.props.palette;
+    const { paletteName, emoji, id } = this.props.palette;
     const colorBoxes = this._shades.map(color => (
       <ColorBox
-        key={color.id}
+        key={color.name}
         name={color.name}
         background={color[format]}
         showLink={false}/>
     ))
     return (
-      <div className="Palette">
+      <div className="SingleColorPalette Palette">
         <Navbar handleChange={this.changeFormat} showingAllColors={false}/>
         <div className="Palette-colors">
           {colorBoxes}
+          <div className="go-back ColorBox">
+            <Link to={`/palette/${id}`} className="back-button">Go Back</Link>
+          </div>
         </div>
         <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
