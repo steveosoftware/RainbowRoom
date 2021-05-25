348f90fbc33617b609e8d63d1ee233580feb48c8
SteveO <stinkypiece@yahoo.com>
2021-05-25 11:11:54 -0700

    add delete icon to mini palette

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { withStyles } from '@material-ui/styles';
-import styles from './styles/MiniPaletteStyles';
+import React from "react";
+import { withStyles } from "@material-ui/styles";
+import styles from "./styles/MiniPaletteStyles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-function MiniPalette(props){
-
+function MiniPalette(props) {
   const { classes, paletteName, emoji, colors } = props;
-  const miniColorBoxes = colors.map(c => (
+  const miniColorBoxes = colors.map((c) => (
     <div
       className={classes.miniColor}
       style={{ background: c.color }}
-      key={c.name}></div>
-  ))
-  return(
-
+      key={c.name}
+    ></div>
+  ));
+  return (
     <div className={classes.root} onClick={props.handleClick}>
+      <div className={classes.delete}>
+        <DeleteIcon className={classes.deleteIcon} />
+      </div>
       <div className={classes.colors}>{miniColorBoxes}</div>
-      <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
+      <h5 className={classes.title}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
+      </h5>
     </div>
   );
 }
--- src/styles/MiniPaletteStyles.js
+++ src/styles/MiniPaletteStyles.js
@@ -7,18 +7,19 @@ export default {
     padding: "0.5rem",
     position: "relative",
     overflow: "hidden",
-    "&:hover": {
-      cursor: "pointer"
-    }
+    cursor: "pointer",
+    "&:hover $deleteIcon": {
+      opacity: 1,
+    },
   },
   colors: {
     backgroundColor: "#dae1e4",
     height: "150px",
     width: "100%",
     borderRadius: "5px",
-    overflow: "hidden"
+    overflow: "hidden",
   },
-  title:{
+  title: {
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center",
@@ -26,18 +27,33 @@ export default {
     color: "black",
     paddingTop: "0.5rem",
     fontSize: "1rem",
-    position: "relative"
+    position: "relative",
   },
-  emoji:{
+  emoji: {
     marginLeft: ".5rem",
-    fontSize: "1.5rem"
+    fontSize: "1.5rem",
   },
-  miniColor:{
+  miniColor: {
     height: "25%",
     width: "20%",
     display: "inline-block",
     margin: "0 auto",
     position: "relative",
-    marginBottom: "-3.5px"
-  }
-}
+    marginBottom: "-3.5px",
+  },
+  delete: {},
+
+  deleteIcon: {
+    color: "white",
+    backgroundColor: "#eb3d30",
+    width: "20px",
+    height: "20px",
+    position: "absolute",
+    right: "0",
+    top: "0",
+    padding: "10px",
+    zIndex: 10,
+    opacity: 0,
+    transition: "all .3s ease-in-out",
+  },
+};
