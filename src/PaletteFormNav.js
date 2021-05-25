92760e6e6088d82eaf7650f92929a6dcadf2b567
SteveO <stinkypiece@yahoo.com>
2021-05-25 10:41:57 -0700

    style tweaks to form and fix nav flex direction issue

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import useStyles from "./styles/PaletteFormNavStyles";
 import PaletteMetaForm from "./PaletteMetaForm";
 import { Link } from "react-router-dom";
 import clsx from "clsx";
@@ -7,47 +7,9 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import ColorLensIcon from "@material-ui/icons/ColorLens";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
-
-const drawerWidth = 400;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  appBar: {
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    height: "64px",
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(["margin", "width"], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  navBtns: {
-    marginRight: "1rem",
-  },
-  button: {
-    margin: "0 0.5rem",
-  },
-  link: {
-    textDecoration: "none",
-  },
-}));
 
 function PaletteFormNav(props) {
   const classes = useStyles();
@@ -80,7 +42,7 @@ function PaletteFormNav(props) {
             edge="start"
             className={clsx(classes.menuButton, open && classes.hide)}
           >
-            <MenuIcon />
+            <ColorLensIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
             Create A Palette
