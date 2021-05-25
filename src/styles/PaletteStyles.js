5f7b514b1035164804019057b63c7bdb797152fd
SteveO <stinkypiece@yahoo.com>
2021-05-25 13:02:53 -0700

    make colorbox responsive

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,4 +1,4 @@
-
+import sizes from "./sizes";
 // eslint-disable-next-line
 export default {
   Palette: {
@@ -10,15 +10,15 @@ export default {
     height: "90%",
   },
   goBack: {
-      width: "20%",
-      height: "50%",
-      margin: "0 auto",
-      display: "inline-block",
-      position: "relative",
-      cursor: "pointer",
-      marginBottom: "-3.5px",
-      opacity: 1,
-      backgroundColor: "black",
+    width: "20%",
+    height: "50%",
+    margin: "0 auto",
+    display: "inline-block",
+    position: "relative",
+    cursor: "pointer",
+    marginBottom: "-3.5px",
+    opacity: 1,
+    backgroundColor: "black",
     "& a": {
       width: "100px",
       height: "30px",
@@ -37,6 +37,18 @@ export default {
       textTransform: "uppercase",
       border: "none",
       textDecoration: "none",
-    }
-  }
+    },
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: "33.3333%",
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: "20%",
+    },
+    [sizes.down("xs")]: {
+      width: "100%",
+      height: "10%",
+    },
+  },
 };
