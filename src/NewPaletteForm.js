177feadab753bc16ce9d9b7de35d9a77c02179fd
SteveO <stinkypiece@yahoo.com>
2021-05-26 05:38:46 -0700

    fix drag delete but in draggable color box

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -168,6 +168,7 @@ export default function NewPaletteForm(props) {
           removeColor={removeColor}
           axis="xy"
           onSortEnd={onSortEnd}
+          distance={20}
         />
       </main>
     </div>
